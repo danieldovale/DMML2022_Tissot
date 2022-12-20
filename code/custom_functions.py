26cb840568411ea2f09a6844bf1d4f5902cc62c0
GiacomoRattazzi <114418718+GiacomoRattazzi@users.noreply.github.com>
2022-12-20 20:16:41 +0100

    Update custom_functions.py

--- code/custom_functions.py
+++ code/custom_functions.py
@@ -2,6 +2,19 @@ from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, r
 import seaborn as sns
 import pandas as pd
 from google.colab import files
+from tqdm import tqdm
+from nltk.corpus import stopwords        
+from nltk.tokenize import word_tokenize, sent_tokenize 
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+import string
+
+import spacy
+from spacy import displacy
+sp = spacy.load("fr_core_news_sm")
 
 
 def evaluate(true, pred):
@@ -24,3 +37,61 @@ def prediction(data, name, download = False):
     if download == True:
       files.download(file_name)
     return df.head()
+
+def spacy_tokenizer(sentence):
+    doc = sp(sentence)
+    stop_words = nltk.corpus.stopwords.words("french")
+    punctuations = string.punctuation
+  
+    mytokens = [ word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in doc ]
+    mytokens = [ word for word in mytokens if word not in punctuations and word not in stop_words ]
+    return mytokens
+
+def get_info(df):
+ 
+    text_length = []                          
+    number_of_sentences = []
+    number_of_words = []
+    sent_length_avg = []
+    words_length_avg = []
+    number_of_words_after_lemma_stop = []
+    longest_word_size = []
+    
+    for text in tqdm(df['sentence'].values):
+        
+      initial_length = len(text)
+      text_length.append(initial_length)
+
+      num_sentences = len(sent_tokenize(text))
+      number_of_sentences.append(num_sentences)
+        
+      punctuations = string.punctuation
+      text2 = text.lower()
+      text2 = word_tokenize(text2)
+      text2 = [word for word in text2 if word not in punctuations]
+      num_words = len(text2)
+      number_of_words.append(num_words)
+
+      sent_length_avg.append(num_words/num_sentences)
+        
+      words_length_avg.append(initial_length/num_words)
+
+      text = sp(text)
+      text = [word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in text]
+      text = [word for word in text if not word in spacy.lang.fr.stop_words.STOP_WORDS and word not in punctuations]
+
+      num_words_after_lemma_stop = len(text)
+      number_of_words_after_lemma_stop.append(num_words_after_lemma_stop)
+
+      word_len = [len(w) for w in text2]
+      longest_word_size.append(np.max(word_len))
+        
+    final_df = pd.concat([pd.Series(text_length), pd.Series(number_of_sentences),
+                             pd.Series(number_of_words), pd.Series(sent_length_avg),
+                             pd.Series(words_length_avg), pd.Series(number_of_words_after_lemma_stop),
+                             pd.Series(longest_word_size)], axis = 1)
+    final_df.columns = ["text_length", "number_of_sentences", "number_of_words",
+                           "sent_length_avg", "words_length_avg",
+                           "number_of_words_after_lemma_stop", "longest_word_size"]
+    
+    return final_df
