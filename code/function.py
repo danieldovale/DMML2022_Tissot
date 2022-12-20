f0501c0584de3c7d3a904e4372a04ec27a5db4e7
GiacomoRattazzi <114418718+GiacomoRattazzi@users.noreply.github.com>
2022-12-20 15:39:54 +0100

    Update function.py

--- code/function.py
+++ code/function.py
@@ -1,2 +1,11 @@
-def testingF(x,f):
-	return x+f
+def evaluate(true, pred):
+    precision = precision_score(true, pred, average = 'weighted')
+    recall = recall_score(true, pred, average = 'weighted')
+    f1 = f1_score(true, pred, average = 'weighted')
+    acc = accuracy_score(true, pred)
+    index = 'result'
+    d = {'accuracy': round(acc,4), 'precision': round(precision,4), 'recall': round(recall,4), 'f1 score': round(f1,4) }
+    df = pd.DataFrame(d,index=["results"])
+    sns.heatmap(pd.DataFrame(confusion_matrix(true, pred)), annot=True, cmap='Oranges', fmt='.7g');
+    return df
+
